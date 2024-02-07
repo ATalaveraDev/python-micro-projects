dac3d9e6a6c3439850fb83a2fb7d21f976af21f0
Angel Talavera <ata@loriot.io>
2024-02-07 11:35:38 +0100

    Pong first commit

---
+++ Pong/main.py
@@ -0,0 +1,25 @@
+
+from turtle import Screen
+from paddle import Paddle
+
+screen = Screen()
+
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+screen.listen()
+
+paddle_right = Paddle((350, 0))
+paddle_left = Paddle((-350, 0))
+
+screen.onkey(paddle_right.move_up, "Up")
+screen.onkey(paddle_right.move_down, "Down")
+screen.onkey(paddle_left.move_up, "w")
+screen.onkey(paddle_left.move_down, "s")
+
+game_is_on = True
+while game_is_on:
+  screen.update()
+
+screen.exitonclick()
