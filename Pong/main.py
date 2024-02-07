3b13c94dbd09b69f13455d404d27742ae3f08517
Angel Talavera <ata@loriot.io>
2024-02-07 12:07:06 +0100

    Collision with paddles implemented

--- Pong/main.py
+++ Pong/main.py
@@ -2,6 +2,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+import time
 
 screen = Screen()
 
@@ -13,6 +14,7 @@ screen.listen()
 
 paddle_right = Paddle((350, 0))
 paddle_left = Paddle((-350, 0))
+ball = Ball()
 
 screen.onkey(paddle_right.move_up, "Up")
 screen.onkey(paddle_right.move_down, "Down")
@@ -21,6 +23,15 @@ screen.onkey(paddle_left.move_down, "s")
 
 game_is_on = True 
 while game_is_on:
+  time.sleep(0.1)
   screen.update()
+  ball.move()
+
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    ball.bounce_y()
+
+  if ball.distance(paddle_right) < 50 and ball.xcor() > 340 or ball.distance(paddle_left) < 50 and ball.xcor() < -340:
+    ball.bounce_x()
+
 
 screen.exitonclick()
